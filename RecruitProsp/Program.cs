1330b609cfbdc1bcacda74c332d0750d0c9a8f98
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-07-04 11:14:14 -0400

    Add project files.

---
+++ RecruitProsp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitProsp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
